5acfb1fcecf814ccb926ad6d5f656ca9021459cc
JOALYSSON FERNANDES <acofernandes@outlook.com>
2024-03-10 12:49:04 -0300

    1º commit teste 1

---
+++ frontend/teste1/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import './components/NivelFacil';
+import NivelFacil from './components/NivelFacil';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <NivelFacil/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/teste1/src/components/NivelFacil.js
@@ -0,0 +1,17 @@
+const NivelFacil = () =>{
+
+    return (
+            <div Class="flex items-center justify-center h-screen">                 
+                    <div Class="text-center">                                          
+                        
+                        <button class=" hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">
+                            <p><a href="https://www.linkedin.com/in/joalyssonfernandes/" target="_blank">JOALYSSON FERNANDES DOS SANTOS </a> </p>                            
+                        </button>
+                    </div>
+            </div>
+    )
+
+};
+
+export default NivelFacil;
+
